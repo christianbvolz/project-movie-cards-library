c46ff55aeac772f36e33fa45689e89badae8cf13
Christian Volz <christianbvolz@gmail.com>
2021-09-01 20:27:10 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import MovieList from './components/MovieCard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++
@@ -1,1 +0,0 @@
-// implementd MovieCard component here
