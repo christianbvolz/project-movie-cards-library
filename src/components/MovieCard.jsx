44218afce7da7e4b82e66a16154ab061e14db3fe
Christian Volz <christianbvolz@gmail.com>
2021-09-01 22:23:07 -0300

    requisito 11, 12, 13 e 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
       <section>
-        <h4>
-          {movie.title}
-        </h4>
-        <h5>
-          {movie.subtitle}
-        </h5>
-        <p>
-          {movie.storyline}
-        </p>
-        <h2>
-          {movie.rating}
-        </h2>
+        <h4>{movie.title}</h4>
+        <h5>{movie.subtitle}</h5>
+        <p>{movie.storyline}</p>
+        <Rating rating={ movie.rating } />
         <img src={ movie.imagePath } alt={ movie.title } />
       </section>
     );
